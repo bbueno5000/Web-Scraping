e26ab2619422acf46f371535a373cf884b7e009c
B. Bueno <bbueno25@hotmail.com>
2020-03-06 01:20:16 -0700

    Tables and XML - Web scraping with Beautiful Soup 4 p.3

--- web_scraping/web_scraping.py
+++ web_scraping/web_scraping.py
@@ -2,11 +2,16 @@
 DOCSTRING
 """
 import bs4
+import pandas
 import urllib.request as request
 
 URL = 'https://pythonprogramming.net/parsememcparseface/'
 SOURCE = request.urlopen(URL).read()
 SOUP = bs4.BeautifulSoup(SOURCE, 'lxml')
+
+DATAFRAMES = pandas.read_html(URL, header=0)
+for DATAFRAME in DATAFRAMES:
+    print(DATAFRAME)
 
 def navigating_tags():
     """
@@ -23,5 +28,25 @@ def navigating_tags():
     for div in SOUP.find_all('div', class_='body'):
         print(div.text)
 
+def tables():
+    """
+    DOCSTRING
+    """
+    table = SOUP.find('table')
+    table_rows = table.find_all('tr')
+    for table_row in table_rows:
+        table_data = table_row.find_all('td')
+        row = [i.text for i in table_data]
+        print(row)
+
+def xml():
+    """
+    DOCSTRING
+    """
+    source = request.urlopen('https://pythonprogramming.net/sitemap.xml').read()
+    soup = bs4.BeautifulSoup(source, 'xml')
+    for url in soup.find_all('loc'):
+        print(url.text)
+
 if __name__ == '__main__':
-    navigating_tags()
+    xml()
