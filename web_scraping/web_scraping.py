6f5db8ef9d92ecb65f712e0ea988e9c063f34b5e
B. Bueno <bbueno25@hotmail.com>
2020-03-05 22:56:21 -0700

    Web scraping and parsing with Beautiful Soup & Python Introduction p.1

--- web_scraping/web_scraping.py
+++ web_scraping/web_scraping.py
@@ -1,1 +1,8 @@
+import bs4
+import urllib.request
 
+url = 'https://pythonprogramming.net/parsememcparseface/'
+source = urllib.request.urlopen(url).read()
+soup = bs4.BeautifulSoup(source, 'lxml')
+for url in soup.find_all('a'):
+    print(url.get('href'))
